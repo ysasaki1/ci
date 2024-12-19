614a2c56e42cbba14f023bebff0a12ec1df8c8da
ysasaki1 <ysasaki1@hawk.iit.edu>
2024-12-20 01:06:38 +0900

    Update user.js

--- user.js
+++ user.js
@@ -57,38 +57,6 @@ const languageData = {
     }
 };
 
-// 言語データの定義
-const languageData = {
-    en: {
-        title: "Vlog Information Management",
-        minorInfo: "Minor Information",
-        vlogInfo: "Monetized Vlog Information",
-        addMinor: "Add Minor",
-        addVlog: "Add Vlog",
-        downloadCSV: "Download CSV",
-        logout: "Logout",
-        vlogTitle: "Vlog Title",
-        totalEarnings: "Total Earnings",
-        totalDuration: "Total Duration (minutes)",
-        registeredMinors: "Registered Minors",
-        registeredVlogs: "Registered Vlogs",
-    },
-    ja: {
-        title: "ブイログ情報管理",
-        minorInfo: "未成年者の情報",
-        vlogInfo: "収益化ブイログ情報",
-        addMinor: "追加",
-        addVlog: "追加",
-        downloadCSV: "CSV出力",
-        logout: "ログアウト",
-        vlogTitle: "ブイログタイトル",
-        totalEarnings: "総収益",
-        totalDuration: "総出演時間 (分)",
-        registeredMinors: "登録された未成年者",
-        registeredVlogs: "登録されたブイログ",
-    }
-};
-
 // 現在の言語を設定
 let currentLanguage = 'ja'; // 初期言語
 
